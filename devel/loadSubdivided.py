4fbf9f8eba599dfc4c160ec6f7ebb5f10c7627a3
Tianpeng Zhang <tzhang@g.harvard.edu>
2023-01-19 17:32:57 -0500

    Append the side faces positions to the end of the list.

--- devel/loadSubdivided.py
+++ devel/loadSubdivided.py
@@ -1,18 +1,11 @@
 import pickle as pkl
 import numpy as np
 
-from geometry_msgs.msg import PoseStamped
-
 with open('subdived.txt') as f:
 	lines = np.array([[float(s) for s in l.strip().split(' ')] for l in f.readlines()])
 
-side_faces = []
-for l in lines:
-	p = PoseStamped()
-	p.pose.position.x = l[0]
-	p.pose.position.y = l[1]
-	p.pose.position.z = l[2]
-	side_faces.
+
+
 with open('../data/side_faces.pkl','wb') as f:
-	pkl.dump(lines,f)
+	pkl.dump(np.array(lines),f)
 
--- scripts/RobotArmServer.py
+++ scripts/RobotArmServer.py
@@ -49,7 +49,8 @@ class GoToServer:
     # Initialize the waypoints to go to.
 
     # Assume the corners are already stored.
-    fn = '{}/data/corners.pkl'.format(PACKAGE_HOME) or input('Please input the path to the .pkl file storing the corners[default:../data/corners.pkl]:')
+    fn = '{}/data/corners.pkl'.format(PACKAGE_HOME) 
+    # or input('Please input the path to the .pkl file storing the corners[default:../data/corners.pkl]:')
     with open(fn,'rb') as f:
         data = pkl.load(f)
         corners = data['corner_poses']
@@ -62,16 +63,27 @@ class GoToServer:
     aq_region = AcquisitionRegion(corner_pos)
     grid = aq_region.grid(grid_d)
 
+    # Assume the side faces are already stored.
+    side_faces_path = '{}/data/side_faces.pkl'.format(PACKAGE_HOME)
+    with open(side_faces_path,'rb') as f:
+      side = pkl.load(f)
+
+    print(side)
+    grid = np.vstack([grid,side])
+
     initial_wp = corners[0]
     waypoints = [copy.deepcopy(initial_wp) for _ in range(len(grid))]
     # The waypoints all have the same orientation as the first corner.
     # The positions of the waypoints are determined by the grid variable.
-    for i in range(len(waypoints)):
+    for i in range(len(grid)):
         waypoints[i].position.x = grid[i,0]
         waypoints[i].position.y = grid[i,1]
         waypoints[i].position.z = grid[i,2]   
 
+
     waypoints = corners + waypoints
+
+
     self.waypoints = waypoints
     self.initial_wp = initial_wp  
     self.corners = corners
